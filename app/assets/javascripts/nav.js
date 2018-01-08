f481e60d56743b9e4e5298241d8da5d352d340ca
Rachel Prisock <rachelprisock@gmail.com>
2018-01-07 19:56:59 -0700

    didn't realize that bootstrap 4 doesn't use col-xs anymore just col-

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -4,14 +4,14 @@
 $( document ).on('turbolinks:load', function() {
     $('#menu-bar').click(function(){
         var left = $('#main-content');
-        if(left.hasClass('col-md-10 col-xs-10'))
+        if(left.hasClass('col-md-10 col-10'))
         {
             $('#sidebar-menu').toggle("slide", { direction: "left" }, 250);
-            left.removeClass("col-md-10 col-xs-10").addClass("col-md-12 col-xs-12");
+            left.removeClass("col-md-10 col-10").addClass("col-md-12 col-12");
         }
         else
         {
-            left.removeClass("col-md-12 col-xs-12").addClass("col-md-10 col-xs-10");
+            left.removeClass("col-md-12 col-12").addClass("col-md-10 col-10");
             $('#sidebar-menu').toggle("slide", { direction: "left" }, 250);
         }
 
