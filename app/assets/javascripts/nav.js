a644285ff7d1e89bc7defd41d9b48462f3de3bdb
Rachel Prisock <rachelprisock@gmail.com>
2018-01-07 19:53:53 -0700

    add xs columns for easier mobile use

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -2,19 +2,16 @@
  * Created by rachelprisock on 1/6/18.
  */
 $( document ).on('turbolinks:load', function() {
-    console.log("woo page is ready");
     $('#menu-bar').click(function(){
         var left = $('#main-content');
-        if(left.hasClass('col-md-10'))
+        if(left.hasClass('col-md-10 col-xs-10'))
         {
-            console.log("Updating from 10 to 12");
             $('#sidebar-menu').toggle("slide", { direction: "left" }, 250);
-            left.removeClass("col-md-10").addClass("col-md-12");
+            left.removeClass("col-md-10 col-xs-10").addClass("col-md-12 col-xs-12");
         }
         else
         {
-            console.log("Updating from 12 to 10");
-            left.removeClass("col-md-12").addClass("col-md-10");
+            left.removeClass("col-md-12 col-xs-12").addClass("col-md-10 col-xs-10");
             $('#sidebar-menu').toggle("slide", { direction: "left" }, 250);
         }
 
